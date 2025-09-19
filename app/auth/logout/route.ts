e8f0fcc41269a1ce2981c6337592cac6c73874aa
carlosalv12 <carlosalcam01@gmail.com>
2025-09-19 13:35:01 +0200

    Arreglo cerrar sesion

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -5,5 +5,5 @@ import { cookies } from 'next/headers'
 export async function GET() {
   const supabase = createRouteHandlerClient({ cookies })
   await supabase.auth.signOut()
-  return NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_SITE_URL))
+  return NextResponse.redirect(new URL('/auth/login', process.env.NEXT_PUBLIC_SITE_URL))
 }
